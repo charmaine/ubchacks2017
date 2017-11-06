b5c8530f17b8249d212390cc9071e1a401cf1875
charmaine <charmainekmlee@yahoo.ca>
2017-11-05 22:29:55 -0800

    sped up js scrolling by calculating offset of h1 element

--- js/main.js
+++ js/main.js
@@ -17,24 +17,10 @@ $(document).ready(function(){
     		}
     	});
 
-      var hashTagActive = "";
-        $(".scroll").on("click touchstart" , function (event) {
-            if(hashTagActive != this.hash) { //this will prevent if the user click several times the same link to freeze the scroll.
-                event.preventDefault();
-                //calculate destination place
-                var dest = 0;
-                if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
-                    dest = $(document).height() - $(window).height();
-                } else {
-                    dest = $(this.hash).offset().top;
-                }
-                //go to destination
-                $('html,body').animate({
-                    scrollTop: dest
-                }, 2000, 'swing');
-                hashTagActive = this.hash;
-            }
-        });
+      $(window).scroll(function() {
+        var $h1 = $("h1");
+        var window_offset = $h1.offset().top - $(window).scrollTop();
+      });
 
   wow = new WOW(
         {
