742d0cb28dbc03e678c7d699b71016d2a85f1b9a
charmaine <charmainekmlee@yahoo.ca>
2017-11-05 20:05:48 -0800

    web img, fixed console error, changed background

--- js/main.js
+++ js/main.js
@@ -17,11 +17,24 @@ $(document).ready(function(){
     		}
     	});
 
-  $(".scroll").click(function(event){
-  	event.preventDefault();
-  	$('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
-
-  });
+      var hashTagActive = "";
+        $(".scroll").on("click touchstart" , function (event) {
+            if(hashTagActive != this.hash) { //this will prevent if the user click several times the same link to freeze the scroll.
+                event.preventDefault();
+                //calculate destination place
+                var dest = 0;
+                if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
+                    dest = $(document).height() - $(window).height();
+                } else {
+                    dest = $(this.hash).offset().top;
+                }
+                //go to destination
+                $('html,body').animate({
+                    scrollTop: dest
+                }, 2000, 'swing');
+                hashTagActive = this.hash;
+            }
+        });
 
   wow = new WOW(
         {
